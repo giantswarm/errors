de6b05bd1f1978418c2ab4a85850eac0262e4893
Tim Schindler <xh3b4sd@gmail.com>
2018-07-23 10:16:41 +0200

    added comments

--- guest/error.go
+++ guest/error.go
@@ -8,10 +8,22 @@ import (
 
 var (
 	APINotAvailablePatterns = []*regexp.Regexp{
+		// A regular expression representing DNS errors for the guest API domain.
+		// Also see the following match example.
 		regexp.MustCompile(`dial tcp: lookup .* on .*:53: (no such host|server misbehaving)`),
+		// A regular expression representing EOF errors for the guest API domain.
+		// Also see the following match example.
 		regexp.MustCompile(`Get https://api\..*/api/v1/nodes.* (unexpected )?EOF`),
+		// A regular expression representing EOF errors for the guest API domain.
+		// Also see the following match example.
 		regexp.MustCompile(`[Get|Post] https://api\..*/api/v1/namespaces/*/.* (unexpected )?EOF`),
+		// A regular expression representing TLS errors related to establishing
+		// connections to guest clusters while the guest API is not fully up. Also
+		// see the following match example.
 		regexp.MustCompile(`Get https://api\..*/api/v1/nodes.* net/http: (TLS handshake timeout|request canceled while waiting for connection).*?`),
+		// A regular expression representing the kind of transient errors related to
+		// certificates returned while the guest API is not fully up. Also see the
+		// following match example.
 		regexp.MustCompile(`[Get|Post] https://api\..*: x509: certificate is valid for ingress.local, not api\..*`),
 	}
 )
