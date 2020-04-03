521279223334b309c01ba85ca76ef60c78481960
Tim Schindler <xh3b4sd@gmail.com>
2020-04-03 14:35:59 +0200

    fix error matching for changing error messages

--- tenant/error.go
+++ tenant/error.go
@@ -30,7 +30,7 @@ var (
 		regexp.MustCompile(`[Get|Patch|Post] https://api\..* .* \(Client.Timeout exceeded while awaiting headers\)`),
 		// A regular expression representing the kind of transient errors related to
 		// certificates returned while the tenant API is not fully up.
-		regexp.MustCompile(`[Get|Patch|Post] https://api\..*: x509: (certificate is valid for ingress.local, not api\..*|certificate has expired or is not yet valid.*|certificate signed by unknown authority \(possibly because of "crypto/rsa: verification error" while trying to verify candidate authority certificate.*?\))`),
+		regexp.MustCompile(`[Get|Patch|Post] "?https://api\..*: x509: (certificate is valid for ingress.local, not api\..*|certificate has expired or is not yet valid.*|certificate signed by unknown authority \(possibly because of "crypto/rsa: verification error" while trying to verify candidate authority certificate.*?\))`),
 	}
 )
 
